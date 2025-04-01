ca23582217c84f0c0855819800bfa5e22ad0ff7e
EneaSema <sema.enea1993@gmail.com>
2025-04-01 16:11:17 +0200

    create request about user and km

--- js-biglietto-treno/script.js
+++ js-biglietto-treno/script.js
@@ -2,11 +2,19 @@
 
 // chiedere età al cliente, il numero dei km, costo fisso 0.21*numero km, in base ad età fare sconto
 
-const ageUser = 31;
-console.log(ageUser);
+// creato richiesta età al cliente e relativa conversione in numero e stampa
 
-const distanceUser = 30;
-console.log(distanceUser);
+const requestAgeUser = prompt("Quanti anni hai?");
+const responseAge = parseInt(requestAgeUser);
+console.log(responseAge);
+
+// creato richiesta età al cliente e relativa conversione in numero e stampa
+
+const requestDistanceUser = prompt("Quanti anni hai?");
+const responseDistance = parseInt(requestDistanceUser);
+console.log(responseDistance);
+
+const pricekm = 0.21;
 
 // SVOLGIMENTO
 
